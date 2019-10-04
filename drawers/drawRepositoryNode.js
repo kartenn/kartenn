b54b9bd3293e0063f0d7483274f3164fac33ac0e
Pierre Rolland <roll.pierre@gmail.com>
2019-10-04 11:59:02 +0200

    Round nodes

--- drawers/drawRepositoryNode.js
+++ drawers/drawRepositoryNode.js
@@ -6,10 +6,10 @@ import colors from './colors';
 function drawRepositoryNode(selection, { scale, showLabel = false }) {
   selection
     .append("path")
-    .attr("d", ({ diskUsage }) => getPolygonPath(scale(diskUsage), 6))
+    .attr("d", ({ diskUsage }) => getPolygonPath(scale(diskUsage), 20))
     .attr("stroke", "#589442")
     .attr("stroke-opacity", 0.3)
-    .attr("stroke-width", 3)
+    .attr("stroke-width", 1)
     .style('fill', (d) => { return colors[d.type] ? colors[d.type] : '#589442'; });
 
   if (showLabel) {
@@ -17,7 +17,7 @@ function drawRepositoryNode(selection, { scale, showLabel = false }) {
       .append("text")
       .attr("text-anchor", "middle")
       .attr("stroke", "#ffffff")
-      .attr("stroke-width", 3)
+      .attr("stroke-width", 1)
       .text(({ name }) => name)
       .attr("y", ({ diskUsage = 0 }) => -(5 + scale(diskUsage)))
 
